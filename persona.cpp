98d6722f2321b26d7fb6eda017814655582bee68
MartinAgarcia1710 <martindlcg@gmail.com>
2023-12-23 11:27:57 -0300

    class persona
    class persona lista para armar cliente y empleados con herencia

---
+++ persona.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include "persona.h"
+#include "fecha.h"
+#include "direcion.h"
+
+const char* Persona::getNombreYapellido(){
+    return nombreYapellido;
+}
+const char* Persona::getNumeroTelefono(){
+    return numeroTelefono;
+}
+const char* Persona::getDNI(){
+    return DNI;
+}
+const char* Persona::getEmail(){
+    return email;
+}
+Fecha Persona::getNacimiento(){
+    return nacimiento;
+}
+Direccion Persona::getDomicilio(){
+    return domicilio;
+}
+bool Persona::getEstado(){
+    return estado;
+}
+
+void Persona::setNombreYapellido(const char* nya){
+    strcpy(nombreYapellido, nya);
+}
+void Persona::setNumeroTelefono(const char* nt){
+    strcpy(numeroTelefono, nt);
+}
+void Persona::setDNI(const char* dn){
+    strcpy(DNI, dn);
+}
+void Persona::setEmial(const char* ma){
+    strcpy(email, ma);
+}
+void Persona::setNacimiento(Fecha fn){
+    nacimiento = fn;
+}
+void Persona::setDireccion(Direccion d){
+    domicilio = d;
+}
+void Persona::setEstado(bool e){
+    estado = e;
+}
--- persona.h
+++ persona.h
@@ -1,21 +1,33 @@
 #ifndef PERSONA_H_INCLUDED
 #define PERSONA_H_INCLUDED
 #include "direcion.h"
-
+#include "fecha.h"
 class Persona{
     protected:
         char nombreYapellido[70];
         char numeroTelefono[10];
-        char cuil[15];
+        char DNI[15];
         char email[30];
         Fecha nacimiento;
         Direccion domicilio;
-
+        bool estado;
 
     public:
+        const char* getNombreYapellido();
+        const char* getNumeroTelefono();
+        const char* getDNI();
+        const char* getEmail();
+        Fecha getNacimiento();
+        Direccion getDomicilio();
+        bool getEstado();
 
-
-
+        void setNombreYapellido(const char* nya);
+        void setNumeroTelefono(const char* nt);
+        void setDNI(const char* dn);
+        void setEmial(const char* ma);
+        void setNacimiento(Fecha fn);
+        void setDireccion(Direccion d);
+        void setEstado(bool e);
 };
 
 
